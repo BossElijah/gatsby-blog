c28b86b5b255f35bd5195a71d6e590c7e4765d3c
Elias Bruhn <elias@lfpost.dk>
2021-05-03 14:23:29 +0200

    Create app

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/elias/Sites/2021/react-course/gatsby-blog/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/elias/Sites/2021/react-course/gatsby-blog/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/elias/Sites/2021/react-course/gatsby-blog/src/pages/index.js")),
+  "component---src-pages-my-files-js": preferDefault(require("/Users/elias/Sites/2021/react-course/gatsby-blog/src/pages/my-files.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/elias/Sites/2021/react-course/gatsby-blog/src/pages/page-2.js")),
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/elias/Sites/2021/react-course/gatsby-blog/src/templates/blog-post.js"))
+}
+
---
+++ src/markdown-pages/my-projects.md
@@ -0,0 +1,12 @@
+---
+title: "My Projects"
+date: "2021-03-05"
+description: "This is a list of all Elias' projects. Updated 3 May 2021"
+---
+
+### Here is gonna be a list of all my projects
+
+* First project
+* Second project
+* Third project
+* Fourth project
